68e516abc58ac06bbe1359cba1a6cd32fa4ec1ec
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2025-05-07 10:44:13 -0400

    dstyeryttr

---
+++ Assets/New Folder/Crosshair.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+}
