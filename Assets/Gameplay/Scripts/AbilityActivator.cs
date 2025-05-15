0ea632ecf7f6305b72faf496f5e11549f36cdcdb
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2025-05-15 10:44:23 -0400

    sdfsdf

---
+++ Assets/Gameplay/Scripts/AbilityActivator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityActivator : MonoBehaviour
+{
+    [SerializeField] private List<Abilities> abilities = new List<Abilities>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
